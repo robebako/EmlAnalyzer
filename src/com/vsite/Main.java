abc331a5d3288f326e516ea5aad7b97c3e80bff1
Robert Bakota <robakota@vsite.hr>
2020-09-12 13:14:46 +0200

    Implement save .eml to another directory

--- src/com/vsite/Main.java
+++ src/com/vsite/Main.java
@@ -1,16 +1,14 @@
 package com.vsite;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
 import javax.mail.*;
 import javax.mail.internet.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        String path ="C:\\TEST2";
-        File folder = new File(path);
+        String readPath ="C:\\TEST2";
+        File folder = new File(readPath);
         File [] listOfFiles = folder.listFiles();
         if (listOfFiles != null) {
             for(File file:listOfFiles)
@@ -30,5 +28,27 @@ public class Main {
         System.out.println("From : " + message.getFrom()[0]);
         System.out.println("--------------");
         //System.out.println("Body : " +  message.getContent());
+
+        if(hasAttachments(message))
+            System.out.println("Has attachment!");
+
+        String savePath ="C:\\TEST3";
+        new File(savePath).mkdir();
+        String name = message.getSubject().replaceAll("[:\\\\/*?|<> \"]", "_");
+        savePath += "\\" + name+".eml";
+        OutputStream dest = new FileOutputStream(new File(savePath));
+        try {
+            message.writeTo(dest);
+        }
+        finally {
+            if (dest != null) { dest.flush(); dest.close(); }
+        }
+    }
+    public static boolean hasAttachments(Message msg) throws MessagingException, IOException {
+        if (msg.isMimeType("multipart/mixed")) {
+            Multipart mp = (Multipart)msg.getContent();
+            return mp.getCount() > 1;
+        }
+        return false;
     }
 }
